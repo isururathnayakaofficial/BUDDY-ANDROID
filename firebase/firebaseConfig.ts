1162904054610ad969e2db3eb847e79c9a61f8a1
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-07-11 10:12:51 +0530

    refact:remove the json configurations for android

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6exDMM8HSUVvi1B74eZFbIm0SPnM2vWA",
+  authDomain: "amd-pro-c026e.firebaseapp.com",
+  projectId: "amd-pro-c026e",
+  storageBucket: "amd-pro-c026e.firebasestorage.app",
+  messagingSenderId: "866189210649",
+  appId: "1:866189210649:web:e79dee5057bb2cba29e688"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
